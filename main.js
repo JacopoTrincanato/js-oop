ec5b87c96996577d91ae569f3903118d440911e9
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-22 15:52:52 +0200

    create calcolaEta method and add it to fiatBlu

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log('hello');
 /*In questo esercizio, dovrai creare una classe in JavaScript chiamata "Veicolo" con alcune proprietà e metodi.
 Successivamente:
-Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
 Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()*/
 
 //creo una classe "Veicolo"
@@ -25,12 +24,23 @@ class Veicolo {
         let info = `${this.marca} ${this.anno} ${this.colore} ${this.porte} ${this.carburante}`
         return info
     }
+
+    //Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
+    calcolaEta(){
+        let data = new Date();
+        let annoCorrente = data.getFullYear();
+        let eta = annoCorrente - this.anno;
+        return eta;
+        
+    }
 }
 
 //creo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
 const fiatBlu = new Veicolo('Fiat', 2019, 'blu', 5, 'benzina');
 console.log(fiatBlu);
 
-let infoMacchina = fiatBlu.informazioni()
+let infoMacchina = fiatBlu.informazioni();
 console.log(infoMacchina);
 
+let etaMacchina = fiatBlu.calcolaEta();
+console.log(etaMacchina);
