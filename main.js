4208c7df45d0dd5fb06f74b630a7245a6f07ee35
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-22 14:59:55 +0200

    create Veicolo class

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('hello');
+/*In questo esercizio, dovrai creare una classe in JavaScript chiamata "Veicolo" con alcune proprietà e metodi.
+Successivamente:
+Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
+Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
+Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()*/
+
+//creo una classe "Veicolo"
+class Veicolo {
+    marca;
+    anno;
+    colore;
+    porte;
+    carburante;
+}
